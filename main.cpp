a4c152d6b5e65433878e8c9c7f9928b53144c3e0
unknown <hakobyanarthur401@gmail.com>
2021-08-30 19:37:35 +0300

    Minor fix

--- main.cpp
+++ main.cpp
@@ -1,37 +1,34 @@
 #include <stdio.h>
 #include <cmath>
 
-using namespace std;
-
-double a, b, c, x, y;
-bool isLiniar = false;
-int roots = 0;
-
-void inputData();
-void findRoot(double a, double b, double c);
-void printData();
+void inputData(double&, double&, double&);
+void findRoot(double&, double&, double&, double&, double&, int&, bool&);
+void printData(double&, double&, int&, bool&);
 
 int main() {
-    inputData();
-    findRoot(a, b, c);
-    printData();
+    double a, b, c, x, y;
+    int roots = 0;
+    bool isLinear = false;
+    inputData(a, b, c);
+    findRoot(a, b, c, x, y, roots, isLinear);
+    printData(x, y, roots, isLinear);
 }
 
-void inputData() {
+void inputData(double &a, double &b, double &c) {
     printf("Please enter coefficients\n");
     scanf("%lf%lf%lf", &a, &b, &c);
 }
 
-void findRoot(double a, double b, double c) {
+void findRoot(double &a, double &b, double &c, double &x, double &y, int &roots, bool &isLinear) {
     if (a == 0) {
         x = -1 * (c / b);
         roots = 1;
-        isLiniar = true;
+        isLinear = true;
     } else {
-        double discriminant = sqrt(b * b - 4 * a * c);
+        double discriminant = b * b - 4 * a * c;
         if (discriminant >= 0) {
-            x = (-1 * b + sqrt(b * b - 4 * a * c)) / 2 * a;
-            y = (-1 * b - sqrt(b * b - 4 * a * c)) / 2 * a;
+            x = (-1 * b + sqrt(discriminant)) / 2 * a;
+            y = (-1 * b - sqrt(discriminant))/ 2 * a;
             if (x == y) {
                 roots = 1;
             } else {
@@ -41,13 +38,13 @@ void findRoot(double a, double b, double c) {
     }
 }
 
-void printData() {
+void printData(double &x, double &y, int &roots, bool &isLinear) {
     switch (roots) {
         case 0:
             printf("This equality has no roots\n");
             break;
         case 1:
-            if (isLiniar) {
+            if (isLinear) {
                 printf("It is liniar equality. ");    
             }
             printf("Root of equality is %g\n", x);
